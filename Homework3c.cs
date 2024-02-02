8f8ebd92cf558f676e8b8d4317e6f79368ac0ca9
Mason Wester <147356325+MLWester@users.noreply.github.com>
2024-02-02 12:13:03 -0600

    Add files via upload

---
+++ Homework3c.cs
@@ -0,0 +1,32 @@
+﻿namespace Homework3c;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number: ");
+
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int numRows))
+        {
+            PrintPattern(numRows);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input");
+        }
+    }
+     static void PrintPattern(int numRows)
+    {
+        for (int i = 1; i <= numRows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
