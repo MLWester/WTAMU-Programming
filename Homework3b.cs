a8ff41f76ff471b637d4c6b3ab7b9f76585a0abb
Mason Wester <147356325+MLWester@users.noreply.github.com>
2024-02-02 12:11:59 -0600

    Add files via upload

---
+++ Homework3b.cs
@@ -0,0 +1,36 @@
+﻿namespace Homework3b;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number (N): ");
+
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int N))
+        {
+            PrintSquarePattern(N);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input");
+        }
+    }
+    static void PrintSquarePattern(int N)
+    {
+
+
+        for (int i = 1; i <= N; i++)
+        {
+            for (int j = 1; j <= N; j++)
+            {
+               
+                Console.Write("#");
+            }
+
+            
+            Console.WriteLine();
+        }
+    }
+}
