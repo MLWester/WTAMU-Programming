88103e10f507033127630ce86688bb73dc32b6df
Mason Wester <147356325+MLWester@users.noreply.github.com>
2024-02-02 12:08:44 -0600

    Homework 3c

--- Program.cs
+++ Program.cs
@@ -1,35 +1,31 @@
-﻿namespace Homework3b;
+﻿namespace Homework3c;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter a number (N): ");
+        Console.WriteLine("Enter a number: ");
 
         string input = Console.ReadLine();
 
-        if (int.TryParse(input, out int N))
+        if (int.TryParse(input, out int numRows))
         {
-            PrintSquarePattern(N);
+            PrintPattern(numRows);
         }
         else
         {
             Console.WriteLine("Invalid Input");
         }
     }
-    static void PrintSquarePattern(int N)
+     static void PrintPattern(int numRows)
     {
-
-
-        for (int i = 1; i <= N; i++)
+        for (int i = 1; i <= numRows; i++)
         {
-            for (int j = 1; j <= N; j++)
+            for (int j = 1; j <= i; j++)
             {
-               
-                Console.Write("#");
+                Console.Write("*");
             }
 
-            
             Console.WriteLine();
         }
     }
