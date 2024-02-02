9857cb16c0c222a069c59e6e7186af66d4a561d1
Mason Wester <147356325+MLWester@users.noreply.github.com>
2024-02-02 12:10:55 -0600

    Add files via upload

---
+++ Homework3a.cs
@@ -0,0 +1,40 @@
+﻿namespace Homework3a;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number: ");
+
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            if (IsPrime(number))
+            {
+                Console.WriteLine($"{number} is prime.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not prime.");
+            }
+        }
+    }
+    static bool IsPrime(int n)
+    {
+        if (n <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                return false; 
+            }
+        }
+
+        return true; 
+    }
+}
