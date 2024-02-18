d496bc9534d84197f668eec6c4b1d34428b02f04
Mason Wester <147356325+MLWester@users.noreply.github.com>
2024-02-18 11:09:17 -0600

    Add files via upload

---
+++ Homework5a.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework5a;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first integer:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second integer:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int largest = GetLargest(num1, num2);
+        Console.WriteLine("The largest number is: " + largest);
+    }
+
+    static int GetLargest(int num1, int num2)
+    {
+        return Math.Max(num1, num2);
+    }
+}
