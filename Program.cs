5d0b490840ddb93be1f7f81ea24583075732a749
Mason Wester <147356325+MLWester@users.noreply.github.com>
2024-02-18 11:06:44 -0600

    Homework5b

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Homework5a;
+﻿namespace Homework5b;
 
 class Program
 {
@@ -10,8 +10,21 @@ class Program
         Console.WriteLine("Enter the second integer:");
         int num2 = int.Parse(Console.ReadLine());
 
-        int largest = GetLargest(num1, num2);
+        Console.WriteLine("Enter the third integer:");
+        int num3 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the fourth integer:");
+        int num4 = int.Parse(Console.ReadLine());
+
+        int largest = GetLargest(num1, num2, num3, num4);
         Console.WriteLine("The largest number is: " + largest);
+    }
+
+    static int GetLargest(int num1, int num2, int num3, int num4)
+    {
+        int largest1 = GetLargest(num1, num2);
+        int largest2 = GetLargest(num3, num4);
+        return GetLargest(largest1, largest2);
     }
 
     static int GetLargest(int num1, int num2)
